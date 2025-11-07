6ff6fc8219ab53147bdc2d27a7db49a05b43c959
dpikurov <dpikurov@yandex.ru>
2025-11-07 21:59:12 +0300

    fix

--- training/src/__init__.py
+++ training/src/__init__.py
@@ -1,12 +1,13 @@
 """Training package exports."""
 
-from .config import TrainingConfig, load_config
+from .config import ConfigError, TrainingConfig, load_config
 from .data import create_dataloaders, describe_dataset
 from .model import SimpleCNN, build_model
 from .trainer import Trainer
 from .utils import logits_to_probabilities, probabilities_to_predictions, set_seed, setup_logging
 
 __all__ = [
+    "ConfigError",
     "TrainingConfig",
     "load_config",
     "create_dataloaders",
